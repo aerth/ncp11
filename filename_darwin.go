a61cb4b7bbac750ceaddb2f78f189e530043749b
aerth <aerth@riseup.net>
2019-07-25 16:57:09 -0400

    fix build (cant use os.Getenv with const)
    download if not exist but don't build

--- filename_darwin.go
+++ filename_darwin.go
@@ -2,11 +2,15 @@
 
 package main
 
-const (
+import "os"
+
+var (
 	// Used for Tor Browser.  TODO: Check whether this works on macOS.
 	filenameCKBIReplaceSelf   = os.Getenv("HOME") + "/libnssckbi.dylib"
 	filenameCKBIReplaceTarget = os.Getenv("HOME") + "/libnssckbi-namecoin-target.dylib"
+)
 
+const (
 	// Used for system-wide applications.  TODO: Check whether this works
 	// on macOS.
 	filenameCKBIAlongsideTarget = "/usr/local/namecoin/libnssckbi-namecoin-target.dylib"
--- filename_other.go
+++ filename_other.go
@@ -8,13 +8,15 @@ import (
 	"os"
 )
 
-const (
+var (
 	// Tor Browser's "start-tor-browser" script on GNU/Linux targets sets
 	// $HOME to the Tor Browser directory, so we check there for a CKBI
 	// library.  This heuristic doesn't work on Windows.
 	filenameCKBIReplaceSelf   = os.Getenv("HOME") + "/libnssckbi.so"
 	filenameCKBIReplaceTarget = os.Getenv("HOME") + "/libnssckbi-namecoin-target.so"
+)
 
+const (
 	// Used for system-wide applications
 	filenameCKBIAlongsideTarget = "/usr/local/namecoin/libnssckbi-namecoin-target.so"
 )
