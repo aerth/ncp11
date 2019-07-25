c57d75974d603fea9da45a65b7a3704936c43b60
aerth <aerth@riseup.net>
2019-07-25 16:55:24 -0400

    make test.py run

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3
 import ctypes
 lib = ctypes.CDLL("./libnamecoin.so")
-x=21
-print("C_Initialize:", lib.C_Initialize())
-print("C_GetInfo:", lib.C_GetInfo())
 print("C_Initialize:", lib.C_Initialize())
 print("C_GetSlotList:", lib.C_GetSlotList())
 print("C_GetTokeninfo:", lib.C_GetTokenInfo())
 print("C_OpenSession:", lib.C_OpenSession())
 print("C_GetMechanismList:", lib.C_GetMechanismList())
-print("C_GetFunctionList:", lib.C_GetFunctionList())
+#print("C_GetFunctionList:", lib.C_GetFunctionList())
+#print("C_GetInfo:", lib.C_GetInfo())
 
